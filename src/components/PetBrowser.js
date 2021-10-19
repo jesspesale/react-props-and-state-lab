e73115ed56d40344964df73bced9189f2d285bcc
Jess Pesale <jess_pesale@aol.com>
2021-10-18 20:02:26 -0400

    commit tree bfacf9fe993a801fa5ddc573679dabb0a571d018

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,7 +4,14 @@ import Pet from './Pet'
 
 class PetBrowser extends React.Component {
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    const pets = this.props.pets.map(pet => {
+      // console.log(pet)
+      return <Pet pet={pet} key={pet.id} onAdoptPet={this.props.onAdoptPet} />
+    })
+    return <div className="ui cards">
+      PET COMPONENT SHOULD GO HERE
+      {pets}
+    </div>
   }
 }
 
